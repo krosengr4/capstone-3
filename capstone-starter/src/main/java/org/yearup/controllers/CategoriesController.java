3016ab5d7098d69499cb2d06638460226c19fa65
krosengr4 <rosenkev4@gmail.com>
2025-06-21 23:53:01 -0500

    Added a controller to update a category

--- capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -84,8 +84,14 @@ public class CategoriesController {
 
    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
    // add annotation to ensure that only an ADMIN can call this function
+   @PutMapping("{id}")
+   @PreAuthorize("hasRole('ROLE_ADMIN')")
    public void updateCategory(@PathVariable int id, @RequestBody Category category) {
-	  // update the category by id
+	  try {
+		 categoryDao.update(id, category);
+	  } catch(Exception e) {
+		 throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+	  }
    }
 
 
