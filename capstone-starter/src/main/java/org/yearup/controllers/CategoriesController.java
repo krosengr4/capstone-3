ae9922b1e5b78a897a9df4218b65835b112fbf18
krosengr4 <rosenkev4@gmail.com>
2025-06-22 00:05:10 -0500

    Added controller to delete a category with admin permission

--- capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -97,7 +97,19 @@ public class CategoriesController {
 
    // add annotation to call this method for a DELETE action - the url path must include the categoryId
    // add annotation to ensure that only an ADMIN can call this function
+   @DeleteMapping("{id}")
+   @PreAuthorize("hasRole('ROLE_ADMIN')")
    public void deleteCategory(@PathVariable int id) {
-	  // delete the category by id
+	  try {
+		 var category = categoryDao.getById(id);
+
+		 if(category == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		 } else {
+			categoryDao.delete(id);
+		 }
+	  } catch (Exception e) {
+		 throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+	  }
    }
 }
