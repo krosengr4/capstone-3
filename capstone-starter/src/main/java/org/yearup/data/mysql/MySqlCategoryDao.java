8d70b0d90a4dc3c96b2f3c0adff53ba410c0ffa1
krosengr4 <rosenkev4@gmail.com>
2025-06-21 23:55:55 -0500

    Created method in MySqlCategoryDao class to delete a category

--- capstone-starter/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ capstone-starter/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -108,7 +108,17 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
 
    @Override
    public void delete(int categoryId) {
-	  // delete category
+	  String query = "DELETE FROM categories " +
+							 "WHERE category_id = ?;";
+
+	  try(Connection connection = getConnection()) {
+		 PreparedStatement statement = connection.prepareStatement(query);
+		 statement.setInt(1, categoryId);
+
+		 statement.executeUpdate();
+	  } catch(SQLException e) {
+		 throw new RuntimeException(e);
+	  }
    }
 
    private Category mapRow(ResultSet row) throws SQLException {
