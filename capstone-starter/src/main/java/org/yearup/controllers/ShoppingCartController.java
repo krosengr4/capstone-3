b5ef9fb66d1e9e1bc80cacf84001bc5d85be3794
krosengr4 <rosenkev4@gmail.com>
2025-06-23 23:10:47 -0500

    Coded feature that allows a user to add a product to their cart

--- capstone-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -2,15 +2,13 @@ package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
 import org.yearup.data.UserDao;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.models.User;
 
 import java.security.Principal;
@@ -37,7 +35,6 @@ public class ShoppingCartController {
    // each method in this controller requires a Principal object as a parameter
    @GetMapping("")
    public ShoppingCart getCart(Principal principal) {
-	  System.out.println("You are in cart controller!");
 	  try {
 		 // get the currently logged in username
 		 String userName = principal.getName();
@@ -54,6 +51,21 @@ public class ShoppingCartController {
 
    // add a POST method to add a product to the cart - the url should be
    // https://localhost:8080/cart/products/15 (15 is the productId to be added
+   @PostMapping("products/{productId}")
+   public ShoppingCartItem addItemToCart(@PathVariable int productId, Principal principal) {
+	  System.out.println("Adding item to the cart!");
+	  try {
+		 //get the user that is logged in
+		 String userName = principal.getName();
+		 User user = userDao.getByUserName(userName);
+		 int userId = user.getId();
+
+		 return shoppingCartDao.addToCart(userId, productId);
+	  } catch(Exception e) {
+		 throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+	  }
+   }
+
 
 
    // add a PUT method to update an existing product in the cart - the url should be
--- capstone-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ capstone-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,8 +1,10 @@
 package org.yearup.data;
 
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 
 public interface ShoppingCartDao {
    ShoppingCart getByUserId(int userId);
    // add additional method signatures here
+   ShoppingCartItem addToCart(int userId, int productId);
 }
