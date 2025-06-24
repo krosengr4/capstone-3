e3c4858142189f947d9c4e7034fcde8e8013ce70
krosengr4 <rosenkev4@gmail.com>
2025-06-23 21:24:41 -0500

    Autowired a constructor into the ShoppingCartController class

--- capstone-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,7 +1,9 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -20,12 +22,20 @@ import java.security.Principal;
 @CrossOrigin
 public class ShoppingCartController {
    // a shopping cart requires
-   private ShoppingCartDao shoppingCartDao;
-   private UserDao userDao;
-   private ProductDao productDao;
+   private final ShoppingCartDao shoppingCartDao;
+   private final UserDao userDao;
+   private final ProductDao productDao;
+
+   @Autowired
+   public ShoppingCartController(ShoppingCartDao shoppingCartDao, UserDao userDao, ProductDao productDao) {
+	  this.shoppingCartDao = shoppingCartDao;
+	  this.userDao = userDao;
+	  this.productDao = productDao;
+   }
 
 
    // each method in this controller requires a Principal object as a parameter
+   @GetMapping("")
    public ShoppingCart getCart(Principal principal) {
 	  try {
 		 // get the currently logged in username
