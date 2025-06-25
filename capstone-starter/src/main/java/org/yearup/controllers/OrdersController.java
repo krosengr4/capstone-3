b065ccf5e1ae7722ddf7ea1cc0a8fd61b3ff1ed9
krosengr4 <rosenkev4@gmail.com>
2025-06-25 13:31:39 -0500

    Added controller method in Orders Controller to GET and display all orders

--- capstone-starter/src/main/java/org/yearup/controllers/OrdersController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/OrdersController.java
@@ -1,11 +1,16 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.yearup.data.OrderDao;
 import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.Order;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("orders")
@@ -18,6 +23,18 @@ public class OrdersController {
    public OrdersController(ShoppingCartDao cartDao, OrderDao orderDao) {
 	  this.cartDao = cartDao;
 	  this.orderDao = orderDao;
+   }
+
+   //GET controller to get all orders. Must be admin to access
+   //GET url = " https://localhost:8080/orders
+   @GetMapping("")
+   @PreAuthorize("hasRole('ROLE_ADMIN')")
+   public List<Order> getAllOrders() {
+	  try {
+		 return orderDao.getAllOrders();
+	  } catch(Exception e) {
+		 throw new RuntimeException(e);
+	  }
    }
 
 }
