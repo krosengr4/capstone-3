f52905f1cacfb4a8d96b396f27c256d9d24921e0
krosengr4 <rosenkev4@gmail.com>
2025-06-25 17:13:26 -0500

    Added method in shoppiingCartDao to get and return a list of shopping cart items in a users cart

--- capstone-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ capstone-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -3,8 +3,12 @@ package org.yearup.data;
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.ShoppingCartItem;
 
+import java.util.List;
+
 public interface ShoppingCartDao {
    ShoppingCart getByUserId(int userId);
+
+   List<ShoppingCartItem> getItemsInCart(int userId);
 
    // add additional method signatures here
    ShoppingCart addToCart(int userId, int productId);
