b39e7bcec2b56ec238324ff304f7bc85c610ed17
krosengr4 <rosenkev4@gmail.com>
2025-06-25 10:11:22 -0500

    Added Orders Controller to implement checkout feature

--- capstone-starter/src/main/java/org/yearup/controllers/OrdersController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/OrdersController.java
@@ -1,10 +1,23 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yearup.data.OrderDao;
+import org.yearup.data.ShoppingCartDao;
 
 @RestController
-@RequestMapping("")
-public class CheckoutController {
+@RequestMapping("orders")
+@CrossOrigin
+public class OrdersController {
+   private final ShoppingCartDao cartDao;
+   private final OrderDao orderDao;
+
+   @Autowired
+   public OrdersController(ShoppingCartDao cartDao, OrderDao orderDao) {
+	  this.cartDao = cartDao;
+	  this.orderDao = orderDao;
+   }
 
 }
