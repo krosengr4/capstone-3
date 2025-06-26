c0dd8d85db65151fc37c361f14ca4f60c9dcdeff
krosengr4 <rosenkev4@gmail.com>
2025-06-26 07:52:02 -0500

    Fixed bug. Bug was if a user had no items in their cart and they clicked checkout button on front end, it would still insert an order into the orders table. Added code to make sure that a user must have items in their cart in order to checkout

--- capstone-starter/src/main/java/org/yearup/controllers/OrdersController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/OrdersController.java
@@ -18,64 +18,68 @@ import java.util.List;
 @RequestMapping("orders")
 @CrossOrigin
 public class OrdersController {
-   private final ShoppingCartDao cartDao;
-   private final OrderDao orderDao;
-   private final UserDao userDao;
-   private final ProfileDao profileDao;
+	private final ShoppingCartDao cartDao;
+	private final OrderDao orderDao;
+	private final UserDao userDao;
+	private final ProfileDao profileDao;
 
-   @Autowired
-   public OrdersController(ShoppingCartDao cartDao, OrderDao orderDao, UserDao userDao, ProfileDao profileDao) {
-	  this.cartDao = cartDao;
-	  this.orderDao = orderDao;
-	  this.userDao = userDao;
-	  this.profileDao = profileDao;
-   }
+	@Autowired
+	public OrdersController(ShoppingCartDao cartDao, OrderDao orderDao, UserDao userDao, ProfileDao profileDao) {
+		this.cartDao = cartDao;
+		this.orderDao = orderDao;
+		this.userDao = userDao;
+		this.profileDao = profileDao;
+	}
 
-   //GET controller to get all items the user that is logged on has ordered
-   //GET url = " https://localhost:8080/orders
-   @GetMapping("")
-   public List<ShoppingCartItem> getItemsOrdered(Principal principal) {
-	  try {
-		 //Get the user that is logged in
-		 String userName = principal.getName();
-		 User user = userDao.getByUserName(userName);
-		 int userId = user.getId();
+	//GET controller to get all items the user that is logged on has ordered
+	//GET url = " https://localhost:8080/orders
+	@GetMapping("")
+	public List<ShoppingCartItem> getItemsOrdered(Principal principal) {
+		try {
+			//Get the user that is logged in
+			String userName = principal.getName();
+			User user = userDao.getByUserName(userName);
+			int userId = user.getId();
 
-		 //Use OrderDao to get list of items the user has ordered
-		 return orderDao.getItemsOrdered(userId);
-	  } catch(Exception e) {
-		 throw new RuntimeException(e);
-	  }
-   }
+			//Use OrderDao to get list of items the user has ordered
+			return orderDao.getItemsOrdered(userId);
+		} catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-   //GET controller add an order to the orders table
-   @PostMapping("")
-   public void addOrder(Principal principal) {
-	  try {
-		 //get the user that is logged in
-		 String userName = principal.getName();
-		 User user = userDao.getByUserName(userName);
-		 int userId = user.getId();
-		 //get the logged in users profile
-		 Profile profile = profileDao.getByUserId(userId);
+	//GET controller add an order to the orders table
+	@PostMapping("")
+	public void addOrder(Principal principal) {
+		try {
+			//get the user that is logged in
+			String userName = principal.getName();
+			User user = userDao.getByUserName(userName);
+			int userId = user.getId();
+			//get the logged in users profile
+			Profile profile = profileDao.getByUserId(userId);
 
-		 //Create new order and set fields
-		 Order order = new Order(0, userId, Date.valueOf(LocalDate.now()), profile.getAddress(), profile.getCity(),
-				 profile.getState(), profile.getZip(), BigDecimal.ZERO); //todo Update shipping price in a future feature
+			//Use cartDao to get all the ShoppingCartItems that are in the users cart
+			List<ShoppingCartItem> cartItems = cartDao.getItemsInCart(userId);
 
-		 //Use orderDao to insert order into orders table
-		 Order addedOrder = orderDao.addOrder(order);
+			if(!cartItems.isEmpty()) {
+				//Create new order and set fields
+				Order order = new Order(0, userId, Date.valueOf(LocalDate.now()), profile.getAddress(), profile.getCity(),
+						profile.getState(), profile.getZip(), BigDecimal.ZERO); //todo Update shipping price in a future feature
 
-		 //Loop through list of items in the cart and use orderDao to insert each item into order_line_items
-		 List<ShoppingCartItem> cartItems = cartDao.getItemsInCart(userId);
-		 for(ShoppingCartItem item : cartItems) {
-			orderDao.insertLineItems(item, addedOrder.getOrderId());
-		 }
+				//Use orderDao to insert order into orders table
+				Order addedOrder = orderDao.addOrder(order);
 
-		 //clear the shopping cart
-		 cartDao.clearCart(userId);
-	  } catch(Exception e) {
-		 throw new RuntimeException(e);
-	  }
-   }
+				//loop through list of users shopping cart items, add each item to order_line_items table in the db
+				for(ShoppingCartItem item : cartItems) {
+					orderDao.insertLineItems(item, addedOrder.getOrderId());
+				}
+
+				//clear the shopping cart
+				cartDao.clearCart(userId);
+			}
+		} catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
