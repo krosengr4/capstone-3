0844b0cb6a8dcf17684667c3ed31c9b41d1ace94
krosengr4 <rosenkev4@gmail.com>
2025-06-26 23:40:26 -0500

    Added comments

--- capstone-starter/src/main/java/org/yearup/models/ShoppingCart.java
+++ capstone-starter/src/main/java/org/yearup/models/ShoppingCart.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ShoppingCart {
+	//ShoppingCart consists of hashmap
    private Map<Integer, ShoppingCartItem> items = new HashMap<>();
 
    public Map<Integer, ShoppingCartItem> getItems() {
@@ -27,8 +28,11 @@ public class ShoppingCart {
 	  return items.get(productId);
    }
 
+   //Get total price of the ShoppingCart
    public BigDecimal getTotal() {
-
+	   //Uses a stream to iterate through each item,
+	   // map each item to the items total
+	   // reduce the list to the sum of all items total
 	  return items.values()
 								 .stream()
 								 .map(i -> i.getLineTotal())
